11660b4672cec43a03c08c40379d3bb9cd639bf4
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2026-01-17 21:46:27 -0300

    request lightning nodes by connectivity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::Deserialize;
+
+const MEMPOOL_API_URL: &str = "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let nodes = reqwest::get(MEMPOOL_API_URL)
+        .await?
+        .json::<Vec<NodeConnectivity>>()
+        .await?;
+
+    nodes.iter().for_each(|node| {
+        println!(
+            "alias: {} ({}), capacity: {}, seen: {}, updated: {}",
+            node.alias, node.public_key, node.capacity, node.first_seen, node.updated_at
+        );
+    });
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct NodeConnectivity {
+    pub public_key: String,
+    pub alias: String,
+    pub capacity: u64,
+    pub first_seen: u64,
+    pub updated_at: i64,
 }
