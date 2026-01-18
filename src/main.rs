a9dd17062701957f3d195a5f41f6bf300abff47c
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2026-01-18 17:15:40 -0300

    periodic update task

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use anyhow::Result;
 use serde::Deserialize;
+use std::time::Duration;
 
+/// The interval at which to update the nodes connectivity data.
+const NODES_CONNECTIVITY_UPDATE_INTERVAL: Duration = Duration::from_secs(30);
 /// The Mempool Space API endpoint for fetching Lightning Network nodes connectivity data.
 const MP_NODES_CONNECTIVITY_API: &str =
     "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let nodes = fetch_nodes_connectivity().await?;
+    // spawn the node connectivity update task.
+    tokio::spawn(async {
+        // by making the loop outside the update function itself, we ensure that any error
+        // in fetching or processing the data does not break future updates.
+        loop {
+            if let Err(err) = update_nodes_connectivity().await {
+                eprintln!("error updating nodes connectivity db: {err}");
+                // we could use exponential backoff here, to let the external service recover
+                // in case of persistent errors, but for now we just retry at the regular interval.
+            }
 
-    nodes.iter().for_each(|node| {
-        println!(
-            "alias: {} ({}), capacity: {}, seen: {}, updated: {}",
-            node.alias, node.public_key, node.capacity, node.first_seen, node.updated_at
-        );
+            tokio::time::sleep(NODES_CONNECTIVITY_UPDATE_INTERVAL).await;
+        }
     });
+    tokio::time::sleep(Duration::from_hours(1)).await;
+
+    Ok(())
+}
+
+/// Updates the local database with the latest connectivity data of Lightning Network nodes.
+async fn update_nodes_connectivity() -> Result<()> {
+    let nodes = fetch_nodes_connectivity().await?;
+    if nodes.is_empty() {
+        anyhow::bail!("fetched zero nodes connectivity data from API");
+    }
+
+    // yes, I do use unwrap in production code, but very consciously. I even always comment why it's
+    // safe to do so like it was an unsafe block. This reassures code reviewers and future me that
+    // it's not an oversight but a deliberate choice, that has been thought through and is guaranteed
+    // to be safe. I do this for any functions that may panic, like unwraps, array indexing,
+    // Vec::swap_remove, etc.
+    // I consider panics to be embarrassing, so no software of mine will ever panic, at most they
+    // will gracefully shut down or degrade functionality.
+    // I also use unwraps a lot at compile time, like for LazyLock initializers with Regexes, that
+    // must be correct or are a programming error that must be fixed before shipping.
+    let max_updated_at = nodes.iter().map(|node| node.updated_at).max().unwrap(); // SAFETY: nodes is not empty.
+    println!("fetched nodes connectivity data: {}", max_updated_at);
+
+    // db store.
+
     Ok(())
 }
 
