adcc04ef0c9ff21cbfe3c3598a76e52d2d61f926
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2026-01-18 16:34:50 -0300

    small refactor to prepare a periodic fetching task

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use anyhow::Result;
 use serde::Deserialize;
 
-const MEMPOOL_API_URL: &str = "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity";
+/// The Mempool Space API endpoint for fetching Lightning Network nodes connectivity data.
+const MP_NODES_CONNECTIVITY_API: &str =
+    "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let nodes = reqwest::get(MEMPOOL_API_URL)
-        .await?
-        .json::<Vec<NodeConnectivity>>()
-        .await?;
+    let nodes = fetch_nodes_connectivity().await?;
 
     nodes.iter().for_each(|node| {
         println!(
@@ -19,6 +18,16 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+/// Fetches the connectivity data of Lightning Network nodes from the Mempool API.
+async fn fetch_nodes_connectivity() -> Result<Vec<NodeConnectivity>> {
+    reqwest::get(MP_NODES_CONNECTIVITY_API)
+        .await?
+        .json()
+        .await
+        .map_err(Into::into)
+}
+
+/// Represents the connectivity information of a Lightning Network node (a subset of the full data).
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct NodeConnectivity {
