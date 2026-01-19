ab1ada934ecdf305c4baea6758ace1d0d9f7f3f2
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2026-01-18 21:07:48 -0300

    include SurrealDB support

--- src/connectivity.rs
+++ src/connectivity.rs
@@ -1,11 +1,14 @@
-use serde::Deserialize;
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use surrealdb::Surreal;
+use surrealdb::engine::any::Any;
 
 /// The Mempool Space API endpoint for fetching Lightning Network nodes connectivity data.
 const NODES_CONNECTIVITY_API: &str =
     "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity";
 
 /// Represents the connectivity information of a Lightning Network node (a subset of the full data).
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NodeConnectivity {
     pub public_key: String,
@@ -16,7 +19,7 @@ pub struct NodeConnectivity {
 }
 
 /// Updates the local database with the latest connectivity data of Lightning Network nodes.
-pub async fn update_nodes_connectivity_task() -> anyhow::Result<()> {
+pub async fn update_nodes_connectivity_task(db: Surreal<Any>) -> Result<()> {
     let nodes = fetch_nodes_connectivity().await?;
     if nodes.is_empty() {
         anyhow::bail!("fetched zero nodes connectivity data from API");
@@ -35,13 +38,24 @@ pub async fn update_nodes_connectivity_task() -> anyhow::Result<()> {
     let max_updated_at = nodes.iter().map(|node| node.updated_at).max().unwrap(); // SAFETY: nodes is not empty.
     println!("fetched nodes connectivity data: {}", max_updated_at);
 
-    // db store.
+    // upsert each node's connectivity data into the database.
+    // this is necessary because they are ranked by connectivity quality (number of open channels),
+    // and the API clips at a fixed number of 100 nodes; this means the returned nodes are likely to
+    // change over time, and thus we need to update already seen ones, as well as insert the others.
+    for node in nodes {
+        let _: Option<NodeConnectivity> = db
+            // yeah, I didn't like the surrealdb upsert API either, this discard with a type is
+            // mandatory so the compiler know which type the .content() method should return.
+            .upsert(("ln_node_connectivity", &node.public_key))
+            .content(node)
+            .await?;
+    }
 
     Ok(())
 }
 
 /// Fetches the connectivity data of Lightning Network nodes from the Mempool API.
-async fn fetch_nodes_connectivity() -> anyhow::Result<Vec<NodeConnectivity>> {
+async fn fetch_nodes_connectivity() -> Result<Vec<NodeConnectivity>> {
     reqwest::get(NODES_CONNECTIVITY_API)
         .await?
         .json()
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 mod connectivity;
 mod utils;
 
+use anyhow::Result;
 use std::time::Duration;
 
 /// The interval at which to update the nodes connectivity data.
-const NODES_CONNECTIVITY_UPDATE_INTERVAL: Duration = Duration::from_secs(30);
+const NODES_CONNECTIVITY_UPDATE_PERIOD: Duration = Duration::from_secs(30);
 
 #[tokio::main]
-async fn main() {
-    // spawn the node connectivity update task.
-    tokio::spawn(utils::periodic_task(
-        NODES_CONNECTIVITY_UPDATE_INTERVAL,
-        connectivity::update_nodes_connectivity_task,
-    )); // ahh, much better! a generic periodic task spawner.
+async fn main() -> Result<()> {
+    // use the endpoint specified in an environment variable, or default to `memory`.
+    let endpoint = std::env::var("SURREALDB").unwrap_or_else(|_| "memory".to_owned());
+    let db = surrealdb::engine::any::connect(endpoint).await?;
+    db.use_ns("namespace").use_db("database").await?;
 
-    tokio::time::sleep(Duration::from_hours(1)).await;
+    // spawn the node connectivity update task.
+    tokio::spawn(utils::periodic_task(NODES_CONNECTIVITY_UPDATE_PERIOD, {
+        let db = db.clone(); // the surreal db handle is cheap to clone, just an Arc internally.
+        move || connectivity::update_nodes_connectivity_task(db.clone())
+    })); // ahh, much better! a generic periodic task spawner.
+
+    // just for demo purposes (and keeping the main function alive), print the current total number
+    // of nodes in the database, that is being updated in the background.
+    loop {
+        let mut nodes = db
+            .query("select count() from ln_node_connectivity group all")
+            .await?;
+        let nodes: Option<u64> = nodes.take("count")?;
+        println!("total nodes in db: {}", nodes.unwrap_or_default());
+        tokio::time::sleep(NODES_CONNECTIVITY_UPDATE_PERIOD).await; // Duration is Copy.
+    }
 }
